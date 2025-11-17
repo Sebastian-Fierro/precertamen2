09900ca14f17681565bca7ef3c4a0d0ac500c48a
sebastian <sebastian.fierro2101@alumnos.ubiobio.cl>
2025-11-17 11:34:40 -0300

    comida

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import Regalos from "./components/Regalos";
+import Comida from "./components/Comida";
 
 
 export function App() {
@@ -8,6 +9,8 @@ export function App() {
       <div style={{ padding: 20 }}>
         <h1>Lista de Regalos</h1>
         <Regalos />
+        <h1>Lista de Comidas</h1>
+        <Comida />
       </div>
     </Fragment>
   );
